74e4e66712d4dbbac9ff6dc88590f2c14ab698e1
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-08 23:14:50 +0200

    added user deletion controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers } from "../database/users";
+import { deleteUserById, getUsers } from "../database/users";
 
 export const getAllUsers = async(req: express.Request, res: express.Response) => {
     try {
@@ -9,6 +9,19 @@ export const getAllUsers = async(req: express.Request, res: express.Response) =>
         return res.status(200).json(users);
     } catch (error) {
         console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+
+        const deletedUser = await deleteUserById(id);
+
+        return res.json(deletedUser);
+    } catch (error) {
+        console.error(error);
         return res.sendStatus(400);
     }
 }
