a234184060a45b73df3115a976df2a14bb96c6b8
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-08 22:48:17 +0200

    updated index.tx middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,1 +1,27 @@
 import express from "express";
+import { get, identity, merge } from "lodash";
+
+import { getUserBySessionToken } from "../database/users";
+
+export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies["VB-AUTH"];
+
+        if (!sessionToken) {
+            return res.sendStatus(403);
+        }
+
+        const existingUser = await getUserBySessionToken(sessionToken);
+
+        if (!existingUser) {
+            return res.sendStatus(403);
+        }
+
+        merge(req, { identity: existingUser });
+
+        return next();
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(400);
+    }
+}
