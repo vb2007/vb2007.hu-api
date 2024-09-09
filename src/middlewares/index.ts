f7f52cc5cd90abda0f243c45c68c777a2492700b
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-09 17:36:47 +0200

    added isOwner to middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,27 @@
 import express from "express";
-import { merge } from "lodash";
+import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../database/users";
+
+export const isOwner = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req,"indentity._id") as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() !== id) {
+            return res.sendStatus(403);
+        }
+
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
+    }
+}
 
 export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
@@ -22,6 +42,6 @@ export const isAuthenticated = async(req: express.Request, res: express.Response
         return next();
     } catch (error) {
         console.error(error);
-        return res.sendStatus(400);
+        return res.sendStatus(500);
     }
 }
