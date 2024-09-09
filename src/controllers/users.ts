1e96816792c94ec7e804d5ddb60c7afc90a53df2
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-09 17:46:56 +0200

    added updateUser, fixed router

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,14 +1,35 @@
 import express from "express";
 
-import { deleteUserById, getUsers } from "../database/users";
+import { deleteUserById, getUserById, getUsers } from "../database/users";
 
-export const getAllUsers = async(req: express.Request, res: express.Response) => {
+export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
 
         return res.status(200).json(users);
     } catch (error) {
         console.log(error);
+        return res.sendStatus(500);
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if (!username) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserById(id);
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.error(error);
         return res.sendStatus(500);
     }
 }
--- src/router/users.ts
+++ src/router/users.ts
@@ -5,5 +5,5 @@ import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
     router.get("/users", isAuthenticated, getAllUsers);
-    router.delete("/users/:id", isOwner, deleteUser);
+    router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
 }
