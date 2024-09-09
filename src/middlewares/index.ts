3e91f8bd4d6eeb36933a0477c60df612d432782b
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-09 18:13:06 +0200

    fixed typo

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,10 +3,10 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../database/users";
 
-export const isOwner = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const { id } = req.params;
-        const currentUserId = get(req,"indentity._id") as string;
+        const currentUserId = get(req,"identity._id") as string;
 
         if (!currentUserId) {
             return res.sendStatus(403);
@@ -23,7 +23,7 @@ export const isOwner = async(req: express.Request, res: express.Response, next: 
     }
 }
 
-export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies["VB-AUTH"];
 
