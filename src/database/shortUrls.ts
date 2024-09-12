afd16ba84bb3ca389e66f89afc781a91a4bd3b61
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-12 21:35:02 +0200

    added functions for url shortening

--- src/database/shortUrls.ts
+++ src/database/shortUrls.ts
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const ShortUrlSchema = new mongoose.Schema({
     originalUrl: {
@@ -18,4 +18,10 @@ const ShortUrlSchema = new mongoose.Schema({
         default: Date.now,
         required: true
     }
-})
+});
+
+export const ShortUrlModel = mongoose.model("ShortUrls", ShortUrlSchema);
+
+export const getOriginalUrl = (shortenedUrl: string) => ShortUrlModel.findOne({ shortenedUrl });
+export const createShortUrl = (values: Record<string, any>) => new ShortUrlModel(values)
+    .save().then((originalUrl) => originalUrl.toObject());
