be17c77dfc4fa200eae3d27a329409210223980c
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-13 21:00:46 +0200

    added url validation function

--- src/controllers/shortenUrls.ts
+++ src/controllers/shortenUrls.ts
@@ -3,9 +3,18 @@ import express from "express";
 import { getOriginalUrl, createShortUrl } from "../database/shortUrls";
 import { authentication } from "../helpers";
 
+function validateUrl(url: string) {
+    if (!url.startsWith("https://") || (!url.startsWith("http://"))) {
+        url = "http://" + url;
+        return url;
+    }
+}
+
 export const shortenUrl = async(req: express.Request, res: express.Response) => {
     try {
         const url = req.body;
+
+        
     } catch (error) {
         console.error(error);
         res.sendStatus(500);
