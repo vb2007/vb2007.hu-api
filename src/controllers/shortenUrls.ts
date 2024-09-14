269447af188fc45970e2b155e76f8348f1ca1975
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-14 16:51:28 +0200

    updated url shortening

--- src/controllers/shortenUrls.ts
+++ src/controllers/shortenUrls.ts
@@ -4,6 +4,18 @@ import { getOriginalUrl, createShortUrl } from "../database/shortUrls";
 
 const containsWhitespace = (text: string) => /\s/.test(text);
 
+function generateShortUrl(length: number) {
+    const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let randomString: string;
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters[randomIndex];
+    }
+
+    return randomString;
+}
+
 export const shortenUrl = async(req: express.Request, res: express.Response) => {
     try {
         const url = req.body;
@@ -12,7 +24,11 @@ export const shortenUrl = async(req: express.Request, res: express.Response) => 
             res.sendStatus(400).json({ error: "The url you've provided is invalid." });
         }
         
-        
+        var shortenedUrl = generateShortUrl(url);
+
+        await createShortUrl(shortenedUrl);
+
+        res.sendStatus(200).json({ shortenedUrl : shortenedUrl })
     } catch (error) {
         console.error(error);
         res.sendStatus(500);
