5a066304592f1bf97a2c0b10a256bd87a05d0dc8
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-02 20:16:40 +0100

    added getting original url to router

--- src/router/shortenUrls.ts
+++ src/router/shortenUrls.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { shortenUrl } from "../controllers/shortenUrls";
+import { redirectToOriginalUrl, shortenUrl } from "../controllers/shortenUrls";
 import { isAuthenticated } from "../middlewares";
 
 export default (router: express.Router) => {
+    router.get("/r/:shortenedUrl", redirectToOriginalUrl);
     router.post("/shortenUrl/create", isAuthenticated, shortenUrl);
 }
