7ff9aa578ad2047cc5e8d3f2df65a2957a6f1a93
VB2007 <admin@vb2007.hu>
2025-04-05 23:49:02 +0200

    added endpoint to router

--- src/router/pastebin.ts
+++ src/router/pastebin.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { createPaste, deletePaste } from "../controllers/pastebin";
+import { createPaste, deletePaste, findPastesByUsername } from "../controllers/pastebin";
 import { isAuthenticated } from "../middlewares";
 
 export default (router: express.Router) => {
     router.post("/paste", isAuthenticated, createPaste);
     router.delete("/paste/:id", isAuthenticated, deletePaste);
+    router.get("/pastes/:username", isAuthenticated, findPastesByUsername);
 }
