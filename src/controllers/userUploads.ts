b64af3b94f30af5b1cbaade8bf37ac797a31d89d
VB2007 <admin@vb2007.hu>
2025-04-25 01:49:27 +0200

    removed duplicate code from upload controller

--- src/controllers/userUploads.ts
+++ src/controllers/userUploads.ts
@@ -7,45 +7,6 @@ import { get } from "lodash";
 import { getUploadById, deleteUploadById, UserUploadsModel } from "../database/userUploads";
 import { upload } from "../helpers/multer";
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const baseUploadDir = path.join(__dirname, process.env.UPLOAD_DISK_DIRECTORY);
-        const currentUsername: string = get(req, 'identity.username') as string;
-
-        const userUploadDir = path.join(baseUploadDir, currentUsername);
-
-        //creates base upload directory if it doesn't exists
-        if (!fs.existsSync(baseUploadDir)) {
-            fs.mkdirSync(baseUploadDir, { recursive: true });
-        }
-
-        //creates user-specific upload directory if it doesn't exists
-        if (!fs.existsSync(userUploadDir)) {
-            fs.mkdirSync(userUploadDir, { recursive: true });
-        }
-
-        cb(null, userUploadDir)
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
-        const fileExt = path.extname(file.originalname);
-        cb(null, uniqueSuffix + fileExt);
-    }
-});
-
-//accepts all files for now, this will later be used for filtering
-const fileFilter = (req: express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    cb(null, true)
-};
-
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        fileSize: parseInt(process.env.UPLOAD_MAX_FILESIZE) * 1024 * 1024,
-    }
-});
-
 export const getUploadDetails = async(req: express.Request, res: express.Response) => {
     try {
         const { id } = req.params;
