8082e19c46487ed97cb2323e3bdc590824c7471c
vb2007 <admin@vb2007.hu>
2025-05-03 23:17:27 +0200

    moved api url and dotenv init to testData

--- src/tests/testData.ts
+++ src/tests/testData.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export class TestData {
-    static readonly username = "testuser";
-    static readonly password = "testpassword";
+    static readonly apiURL: string = process.env.APP_IP + ":" + process.env.APP_PORT;
+    static readonly username: string = "testuser";
+    static readonly password: string = "testpassword";
 }
