0008e13638f2d84825a87cfc8f9563dc2c834dee
vb2007 <admin@vb2007.hu>
2025-05-04 01:15:17 +0200

    updated testData with values read from .env

--- src/tests/testData.ts
+++ src/tests/testData.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export class TestData {
     static readonly apiURL: string = process.env.APP_IP + ":" + process.env.APP_PORT;
-    static readonly username: string = "testuser";
-    static readonly password: string = "testpassword";
+    static readonly email: string = process.env.TEST_EMAIL;
+    static readonly password: string = process.env.TEST_PASSWORD;
 }
