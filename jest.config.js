e37c79661652b37eab71a8d64f05ef8791a3338a
VB2007 <admin@vb2007.hu>
2025-06-22 23:12:16 +0200

    stopped jest from running tests automatically on every save

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testMatch: ["<rootDir>/src/tests/**/*.test.ts"]
+    testMatch: ["<rootDir>/src/tests/**/*.test.ts"],
+    verbose: true,
+    collectCoverage: false,
+    silent: false,
+    reporters: [
+        "default",
+        [
+            "jest-junit",
+            {
+                outputDirectory: "test-results",
+                outputName: "junit.xml",
+                suiteName: "Jest Tests"
+            }
+        ]
+    ],
+    watchman: false
     // setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"]
 };
