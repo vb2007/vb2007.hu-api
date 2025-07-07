e6fc490f18317fca409c7e4568b99117ddb44b09
vb2007 <admin@vb2007.hu>
2025-07-07 07:20:36 +0200

    added controller for url deletion

--- src/controllers/shortenUrls.ts
+++ src/controllers/shortenUrls.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { get } from "lodash";
 
-import { getOriginalUrl, getShortUrl, createShortUrl } from "../database/shortUrls";
+import {
+    getOriginalUrl,
+    getShortUrl,
+    createShortUrl,
+    deleteByShortUrl
+} from "../database/shortUrls";
 import { generateRandomString, validateUri } from "../helpers/text";
 
 export const shortenUrl = async (req: express.Request, res: express.Response) => {
@@ -58,6 +63,27 @@ export const redirectToOriginalUrl = async (req: express.Request, res: express.R
         }
 
         return res.status(302).redirect(response.originalUrl);
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+};
+
+export const deleteUrl = async (req: express.Request, res: express.Response) => {
+    try {
+        const { shortenedUrl } = req.body;
+
+        if (!shortenUrl) {
+            return res.status(400).json({ error: "You must specify a shortened URL." });
+        }
+
+        const response = await deleteByShortUrl(shortenedUrl);
+
+        if (!response) {
+            await res.status(404).json({ error: "The shortened URL cannot be found." });
+        }
+
+        return res.status(200).json({ message: "Shortened URL deleted successfully." });
     } catch (error) {
         console.error(error);
         return res.sendStatus(500);
