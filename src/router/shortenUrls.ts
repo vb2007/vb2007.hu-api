147aca44a90ceaeaf01b6c2ddcb2e22064d0e2ab
vb2007 <admin@vb2007.hu>
2025-07-07 07:38:19 +0200

    added url delete to router

--- src/router/shortenUrls.ts
+++ src/router/shortenUrls.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { redirectToOriginalUrl, shortenUrl } from "../controllers/shortenUrls";
-import { isAuthenticated } from "../middlewares";
+import { redirectToOriginalUrl, shortenUrl, deleteUrl } from "../controllers/shortenUrls";
+import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
     router.get("/r/:shortenedUrl", redirectToOriginalUrl);
     router.post("/shortenUrl/create", isAuthenticated, shortenUrl);
+    router.delete("/shortenUrl/delete", isAuthenticated, isOwner, deleteUrl);
 };
