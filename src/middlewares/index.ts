73017e3d2274e180c384d63581fba5ec08250b5d
vb2007 <admin@vb2007.hu>
2025-07-07 15:59:38 +0200

    changed func order

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,30 +3,6 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../database/users";
 
-export const isOwner = async (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-) => {
-    try {
-        const { id } = req.params;
-        const currentUserId = get(req, "identity._id") as string;
-
-        if (!currentUserId) {
-            return res.sendStatus(403);
-        }
-
-        if (currentUserId.toString() !== id) {
-            return res.sendStatus(403);
-        }
-
-        return next();
-    } catch (error) {
-        console.error(error);
-        return res.status(500);
-    }
-};
-
 export const isAuthenticated = async (
     req: express.Request,
     res: express.Response,
@@ -51,5 +27,29 @@ export const isAuthenticated = async (
     } catch (error) {
         console.error(error);
         return res.sendStatus(500);
+    }
+};
+
+export const isOwner = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, "identity._id") as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() !== id) {
+            return res.sendStatus(403);
+        }
+
+        return next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
     }
 };
