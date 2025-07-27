74e5ccc41b6ca24ed1149dca5b36a082138de8b3
VB2007 <admin@vb2007.hu>
2025-07-28 00:19:53 +0200

    added responses class to contants

---
+++ src/constants/responses.ts
@@ -0,0 +1,20 @@
+export class Responses {
+    static readonly Authentication = class {
+        //Login
+        static readonly missingEmailPassword: string =
+            "You must provide an E-mail address and a password.";
+        static readonly userNotFound: string = "There is no such user with that E-mail address.";
+        static readonly incorrectPassword: string = "The provided password is incorrect.";
+        static readonly loginSuccess: string = "Login successful with user.";
+
+        //Register
+        static readonly missingEmailPasswordUsername: string =
+            "You must provide a username, E-mail address, and a password.";
+        static readonly emailAlreadyExists: string =
+            "An account with this E-mail address already exists.";
+        static readonly usernameTaken: string = "This username is already taken.";
+        static registrationSuccess(username: string): string {
+            return `User ${username} registered successfully.`;
+        }
+    };
+}
