7c0626c2988268e4ea0c51cde8359b689f5e3ba3
VB2007 <admin@vb2007.hu>
2025-07-28 01:05:35 +0200

    used shorten constants in shortenurl controller

--- src/controllers/shortenUrls.ts
+++ src/controllers/shortenUrls.ts
@@ -8,6 +8,7 @@ import {
     deleteByShortUrl
 } from "../database/shortUrls";
 import { generateRandomString, validateUri } from "../helpers/text";
+import { Responses } from "constants/responses";
 
 export const shortenUrl = async (req: express.Request, res: express.Response) => {
     try {
@@ -15,7 +16,7 @@ export const shortenUrl = async (req: express.Request, res: express.Response) =>
         const currentUserId = get(req, "identity._id") as string;
 
         if (!validateUri(url)) {
-            return res.status(400).json({ error: "The URI/URL you've provided isn't valid." });
+            return res.status(400).json({ error: Responses.ShortenUrl.invalidUrl });
         }
 
         const existingShortUrl = await getShortUrl(url);
@@ -32,7 +33,7 @@ export const shortenUrl = async (req: express.Request, res: express.Response) =>
         });
 
         const finalResponse: Object = {
-            message: "URL shortened successfully",
+            message: Responses.ShortenUrl.urlShortenedSuccess,
             data: {
                 shortenedUrl: shortenedUrl.shortenedUrl,
                 originalUrl: shortenedUrl.originalUrl,
@@ -52,16 +53,14 @@ export const redirectToOriginalUrl = async (req: express.Request, res: express.R
     try {
         const { shortenedUrl } = req.params;
 
-        if (!shortenUrl) {
-            return res.status(400).json({ error: "You must specify a shortened URL." });
+        if (!shortenedUrl) {
+            return res.status(400).json({ error: Responses.ShortenUrl.missingShortenedUrl });
         }
 
         const response = await getOriginalUrl(shortenedUrl);
 
         if (!response) {
-            return res
-                .status(404)
-                .json({ error: "The shortened URL you've requested cannot be found." });
+            return res.status(404).json({ error: Responses.ShortenUrl.urlNotFound });
         }
 
         return res.status(302).redirect(response.originalUrl);
@@ -75,17 +74,17 @@ export const deleteUrl = async (req: express.Request, res: express.Response) => 
     try {
         const { shortenedUrl } = req.body;
 
-        if (!shortenUrl) {
-            return res.status(400).json({ error: "You must specify a shortened URL." });
+        if (!shortenedUrl) {
+            return res.status(400).json({ error: Responses.ShortenUrl.missingShortenedUrl });
         }
 
         const response = await deleteByShortUrl(shortenedUrl);
 
         if (!response) {
-            await res.status(404).json({ error: "The shortened URL cannot be found." });
+            await res.status(404).json({ error: Responses.ShortenUrl.urlNotFound });
         }
 
-        return res.status(200).json({ message: "Shortened URL deleted successfully." });
+        return res.status(200).json({ message: Responses.ShortenUrl.urlDeletedSuccess });
     } catch (error) {
         console.error(error);
         return res.sendStatus(500);
