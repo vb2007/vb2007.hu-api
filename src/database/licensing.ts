70ea6cd8c12a07ec3315b33c9aa278574b350960
VB2007 <admin@vb2007.hu>
2025-07-30 03:07:25 +0200

    added licensing db schema 1st concept file

---
+++ src/database/licensing.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const LicenseSchema = new mongoose.Schema({
+    licenseKey: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    usage: {
+        isUsed: {
+            type: Boolean,
+            default: false
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        usedAt: {
+            type: Date
+        }
+    }
+});
