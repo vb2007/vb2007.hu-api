5c67ee76c23ecc87a235df9e0d1904f4433fc60b
VB2007 <admin@vb2007.hu>
2025-08-01 23:57:23 +0200

    made registerApp controller function work

--- src/controllers/licensing.ts
+++ src/controllers/licensing.ts
@@ -1,10 +1,19 @@
 import express from "express";
 
+import { registerApp as registerAppDB } from "../database/licensing";
 import { Responses } from "../constants/responses";
 
 export const registerApp = async (req: express.Request, res: express.Response) => {
     try {
         const { uniqueAppId } = req.body;
+        const user = req.identity;
+
+        const response = await registerAppDB({
+            uniqueAppId: uniqueAppId,
+            userId: user._id
+        });
+
+        return res.status(201).json({ message: "App registered successfully", data: response });
     } catch (error) {
         console.error(error);
         return res.sendStatus(500);
