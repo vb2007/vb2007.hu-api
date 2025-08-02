1e12a0da8e0a8ff61ab08258d75488f593565c6f
VB2007 <admin@vb2007.hu>
2025-08-02 19:27:26 +0200

    added activateLicense to router

--- src/router/licensing.ts
+++ src/router/licensing.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { registerApp, assignLicense } from "../controllers/licensing";
+import { registerApp, assignLicense, activateLicense } from "../controllers/licensing";
 import { isAuthenticated, isSuperUser } from "../middlewares";
 
 export default (router: express.Router) => {
     router.post("/licensing/registerApp", isAuthenticated, registerApp);
     router.post("/licensing/assignLicense", isSuperUser, assignLicense);
+    router.post("/licensing/activateLicense", isAuthenticated, activateLicense);
 };
