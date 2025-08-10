66984ee18a519672b74d71f47af477dcb7a3ec33
VB2007 <admin@vb2007.hu>
2025-08-11 00:06:53 +0200

    added new license getting endpoints to router

--- src/router/licensing.ts
+++ src/router/licensing.ts
@@ -1,9 +1,17 @@
 import express from "express";
 
-import { registerApp, assignLicense, activateLicense } from "../controllers/licensing";
+import {
+    getAllLicenses,
+    getLicense,
+    registerApp,
+    assignLicense,
+    activateLicense
+} from "../controllers/licensing";
 import { isAuthenticated, isSuperUser } from "../middlewares";
 
 export default (router: express.Router) => {
+    router.get("/licensing/licenses", isAuthenticated, isSuperUser, getAllLicenses);
+    router.get("/licensing/license", isAuthenticated, getLicense);
     router.post("/licensing/register", isAuthenticated, registerApp);
     router.post("/licensing/assign/:userId", isAuthenticated, isSuperUser, assignLicense);
     router.post("/licensing/activate", isAuthenticated, activateLicense);
